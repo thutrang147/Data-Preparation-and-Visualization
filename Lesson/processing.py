2e6a2089b13a1a83b2ef31db8391271f2e83bc24
thutrang147 <thutrang1472005@gmail.com>
2026-03-17 21:11:09 +0700

    up code

---
+++ Lesson/processing.py
@@ -0,0 +1,13 @@
+class RemoveStd:
+    pass
+class RemoveIQR:
+    '''
+    remove outlier by IQA
+    '''
+    def __init__(self):
+        pass
+    
+    def fit(self, X):
+        pass
+    def transform(self, X):
+        pass
